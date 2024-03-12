6a0a185c96d31921439cce8900f9fa21f42b15da
majernik <majernik@slac.stanford.edu>
2024-03-11 17:29:58 -0700

    latest

--- runMe.sh
+++ runMe.sh
@@ -2,8 +2,6 @@
 
 cd /u/ra/majernik/git/facet2OfflineDocs
 
-touch /u/ra/majernik/git/facet2OfflineDocs/test
-
 /usr/bin/wget -O "Outage Recovery.pdf" https://confluence.slac.stanford.edu/spaces/flyingpdf/pdfpageexport.action?pageId=428805488
 
 /usr/bin/wget -O "Recovery from unplanned power outage.pdf" https://confluence.slac.stanford.edu/spaces/flyingpdf/pdfpageexport.action?pageId=432642766
